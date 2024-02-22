0e160a5ae91099fbe7246cb899bc6e552e3cf56a
tcarruth <tomsinbox@gmail.com>
2024-02-22 12:59:56 -0800

    First push

--- docs/copyfiles.R
+++ docs/copyfiles.R
@@ -1,15 +1,16 @@
 # Copy files to github io
 
 githubdir = "C:/Github/"
-githubdir = "C:/Users/tcarruth/Documents/Github/"
-frompath = paste0(githubdir,"CDFW_Bag_Limits")
-frompathG = "G:/Shared drives/BM shared/1. Projects/CDFW_halibut_bag_limits"
-topath = paste0(githubdir,"CDFW_Bag_Limits_IO/docs") 
+#githubdir = "C:/Users/tcarruth/Documents/Github/"
+frompath = paste0(githubdir,"lingcod-outside")
+frompathG = "G:/Shared drives/BM shared/1. Projects/DFO_Lingcod"
+topath = paste0(githubdir,"lingcod-outside-io/docs") 
 
 copyall = function(thisdir, todir){
   fromfiles = list.files(thisdir,include.dirs = F)
   for(j in 1:length(fromfiles))  file.copy(paste0(thisdir,fromfiles[j]), paste0(todir,fromfiles[j]),overwrite=T)
 }  
+
   
 copyall(paste0(frompathG,"/References/"), paste0(topath,"/References/"))
 copyall(thisdir=paste0(frompath,"/Figures/Data/"),todir= paste0(topath,"/Figures/"))
